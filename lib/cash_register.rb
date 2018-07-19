18db60ba1031dc80228d2bff4696cc40fde0264c
Ramona M <r1mari@yahoo.com>
2018-07-19 19:36:09 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+  attr_accessor :discount, :total
+def initialize(discount=0)
+  @total = 0
+  @discount = discount
+  @cart = []
+end
 
+def add_item(cart, total)
+  @cart << 
+end
+
+def apply_discount
+  
+end
+def void_last_transaction
+  
+end
+
+
+
+
+end
